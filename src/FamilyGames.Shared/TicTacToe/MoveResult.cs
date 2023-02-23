17c60d30d52f62eeee48baa9cff5a5d6883ddf51
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2023-02-23 00:59:19 -0600

    removed unused projects

--- src/FamilyGames.Shared/TicTacToe/MoveResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FamilyGames.Shared.TicTacToe;
-
-public enum MoveResult
-{
-    None = 0,
-    WinX = 1,
-    WinO = 2,
-    Draw = 3
-}
--- src/FamilyGames.Shared/TicTacToe/PlayerMoveRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FamilyGames.Shared.TicTacToe;
-
-public sealed record PlayerMoveRequest
-{
-    public required Player Player { get; init; }
-    public required int Slot { get; init; }
-}
