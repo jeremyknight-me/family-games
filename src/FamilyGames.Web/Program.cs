c7e3a990f7554ee2945bc1b8af469f4fcaa0a8fa
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-01-04 18:59:10 -0600

    dotnet8 (#6)
    * upgraded to dotnet8
    * upgraded to server side interactive rendering
    * upgraded bootstrap icons
    * upgraded bootstrap

--- src/FamilyGames.Web/Program.cs
+++ src/FamilyGames.Web/Program.cs
@@ -1,26 +1,28 @@
+using FamilyGames.Web.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+
+builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/error", createScopeForErrors: true);
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
