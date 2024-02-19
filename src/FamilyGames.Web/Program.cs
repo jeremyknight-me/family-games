2c5763c5b6d5fc378358f7d0d8d15bdbf51abb33
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-02-19 13:36:49 -0600

    added new blazor project

--- src/FamilyGames.Web/Program.cs
+++ src/FamilyGames.Web/Program.cs
@@ -2,14 +2,10 @@ using FamilyGames.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 if (!app.Environment.IsDevelopment())
 {
---
+++ src/FamilyGames/FamilyGames/Program.cs
@@ -0,0 +1,37 @@
+using FamilyGames.Components;
+
+namespace FamilyGames;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.Run();
+    }
+}
